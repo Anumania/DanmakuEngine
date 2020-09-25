7eca97a35768e020e793ae118cb5194711e110c4
Jonah <jonahmcathcart@gmail.com>
2020-09-25 16:24:33 -0700

    updated monogame version.

---
+++ Objects/Player.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace TestGame.objects
+{
+    class Player : GameObject
+    {
+        int speed = 8;
+        static Texture2D playerSprite;
+        static Texture2D padoru;
+
+        public Player() : base() {
+            Create();
+        }
+
+        public Player(int x, int y) : base(x, y) {
+            Create();
+        }
+
+        public void Create()
+        {
+            scale.X = playerSprite.Width;
+            scale.Y = playerSprite.Height;
+        }
+        public static void LoadContent()
+        {
+            ContentManager content = TestGame.Game1.game.Content;
+            playerSprite = content.Load<Texture2D>("player");
+            padoru = content.Load<Texture2D>("damnMaku");
+            //base.LoadContent(content);
+        }
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(playerSprite, position,Color.White);
+            if(speed == 3)
+            {
+                spriteBatch.Draw(padoru, position + (scale / 2) - (new Vector2(padoru.Width,padoru.Height)/2), Color.White);
+            }
+            //base.Draw(spriteBatch);
+        }
+        public override void Step()
+        {
+            if (Keyboard.GetState().IsKeyDown(Keys.LeftShift)) speed = 3; else speed = 8;
+            if (Keyboard.GetState().IsKeyDown(Keys.J)) position.X-=speed;
+            if (Keyboard.GetState().IsKeyDown(Keys.L)) position.X+=speed;
+            if (Keyboard.GetState().IsKeyDown(Keys.I)) position.Y-=speed;
+            if (Keyboard.GetState().IsKeyDown(Keys.K)) position.Y+=speed;
+            foreach(Danmaku obj in Game1.stepList.OfType<Danmaku>())
+            {
+                int hboxSize = 4;
+                if (GameObject.checkCollision(obj,new Rectangle((position + scale/2).ToPoint() - new Vector2(hboxSize/2).ToPoint(), new Point(hboxSize / 2))))
+                {
+                    foreach(Danmaku obj2 in Game1.stepList.OfType<Danmaku>())
+                    {
+                        Game1.RemoveFromStepList(obj2);
+                    }
+                    break;
+                }
+            }
+            base.Step();
+        }
+    }
+}
